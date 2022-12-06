b7024770beb1e5f09eebf15297888864bae87187
raheelob <raheel.ob@gmail.com>
2022-12-06 19:10:01 +0400

    Currency converter
    1- Currency converter formulae in view model.

--- app/src/main/java/com/example/currencyexchange/ui/currency/viewmodel/CurrencyViewModel.kt
+++ app/src/main/java/com/example/currencyexchange/ui/currency/viewmodel/CurrencyViewModel.kt
@@ -4,14 +4,17 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.currencyexchange.data.api.RemoteData
+import com.example.currencyexchange.data.model.ConversionRatesModel
 import com.example.currencyexchange.ui.currency.event.CurrencyDataEvent
 import com.example.currencyexchange.ui.currency.usecase.CurrencyUseCase
+import com.example.currencyexchange.utils.CurrencyName
 import com.example.currencyexchange.utils.RepoCallType
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.reflect.full.memberProperties
 
 
 @HiltViewModel
@@ -24,7 +27,7 @@ class CurrencyViewModel @Inject constructor(
     internal val currencyExchangeRateTasksEvent = currencyExchangeRateEventChannel.receiveAsFlow()
     /***********************************************************************************/
 
-     fun fetchCurrencyExchangeRate(currency: String) = viewModelScope.launch {
+      fun fetchCurrencyExchangeRate(currency: String, amountToConvert: Double) = viewModelScope.launch {
         currencyExchangeRateEventChannel.send(CurrencyDataEvent.Loading)
         currencyUseCase.execute(CurrencyUseCase.Params(currency = currency, type = RepoCallType.VIEW_MODEL))
             .collect { response ->
@@ -35,6 +38,7 @@ class CurrencyViewModel @Inject constructor(
 
                     is RemoteData.Success -> response.value?.let {
                         Log.d("",""+it.conversionRates)
+                        convertCurrency(CurrencyName.USD.name, CurrencyName.AED.name, it.conversionRates!!, amountToConvert)
                     }
 
                     is RemoteData.RemoteErrorByNetwork -> {
@@ -46,6 +50,13 @@ class CurrencyViewModel @Inject constructor(
                     }
                 }
             }
+    }
+
+    private fun convertCurrency(fromCurrency: String, toCurrency: String, conversionRatesModel: ConversionRatesModel, amountToConvert : Double) {
+        val fromCurrencyObject = ConversionRatesModel::class.memberProperties.find { it.name == fromCurrency }?.get(conversionRatesModel) as Double
+        val toCurrencyObject = ConversionRatesModel::class.memberProperties.find { it.name == toCurrency }?.get(conversionRatesModel) as Double
+        val conversionMoney = (amountToConvert * toCurrencyObject)/fromCurrencyObject
+//        Log.d("Robert","converted money ====>> $conversionMoney")
     }
 
 }
